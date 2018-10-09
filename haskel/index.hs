3aa2b6a808effe9cbaa72585bc053e57f980b4d0
Potapov Dmitriy <potapovd3012@gmail.com>
2018-10-09 22:38:22 +0300

    add guard pattern matching

--- haskel/index.hs
+++ haskel/index.hs
@@ -1,3 +1,23 @@
+add:: Integer -> Integer -> Integer -- declaration
+add x y = x + y                     -- difinition
+-- patern matching
+
+fact :: Integer -> Integer
+
+fact 0 =1
+fact n = n * fact (n-1) -- factorial
+
+
+-- guard statement
+
+guardFact :: Integer -> Integer
+
+guardFact n | n == 0 = 1
+            | n /=0 = n * guardFact(n-1)
+
+
 main = do
-   print(2 :: Int)
-   print(2 :: Float)
+    putStrLn "Strin is here" -- console.log js
+    print(add 2 6) -- console.log
+    print(fact 0)
+    print(guardFact 5)
